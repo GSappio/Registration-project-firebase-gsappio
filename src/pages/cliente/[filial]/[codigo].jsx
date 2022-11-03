7c8328f50c1ad3d8459f18b6a2985fabdaef193c
Gabriel Sappio <100438648+GSappio@users.noreply.github.com>
2022-11-03 12:55:48 -0300

    Component with State

--- src/pages/cliente/[filial]/[codigo].jsx
+++ src/pages/cliente/[filial]/[codigo].jsx
@@ -4,7 +4,7 @@ import { useRouter } from 'next/router';
 export default function ClienteProCodigo() {
 
     const router = useRouter()
-
+    
     return (
         <Layout titulo="Navegação Dinâmica">    
             <div>Código = {router.query.codigo}</div>
